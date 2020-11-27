210b6848f6f71fbaf80f85eea2cbe1a9fc759e56
watpekin <henrique.watpequim@gmail.com>
2020-11-27 18:59:52 -0300

    department implementado

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -43,7 +43,8 @@ public class DepartmentListController implements Initializable {
 
 	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
-		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+		Department obj = new Department();
+		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	@Override
@@ -74,10 +75,15 @@ public class DepartmentListController implements Initializable {
 
 	}
 
-	private void createDialogForm(String absoluteName, Stage patentStage) {
+	private void createDialogForm(Department obj, String absoluteName, Stage patentStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
+			
+			DepartmentFormController controller = loader.getController();
+			controller.setDepartment(obj);
+			controller.updateFormData();
+			
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter Department data");
 			dialogStage.setScene(new Scene(pane));
